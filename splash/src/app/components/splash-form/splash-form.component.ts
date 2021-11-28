58d2a37a314af1183c85f8f384dc4bb1308546dc
Kluckmuck <kluckmucki@gmail.com>
2021-11-28 19:11:39 +0100

    New card and form for Splash

--- splash/src/app/components/splash-form/splash-form.component.ts
+++ splash/src/app/components/splash-form/splash-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-splash-form',
   templateUrl: './splash-form.component.html',
-  styleUrls: ['./splash-form.component.css']
+  styleUrls: ['./splash-form.component.css'],
 })
 export class SplashFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
